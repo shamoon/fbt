afd051a393fbaad4e67e1754a6d4c0cc51514aee
Michael Shamoon <4887959+nikonratm@users.noreply.github.com>
2020-09-02 19:21:13 -0700

    Apple doesnt allow donation links, updated help link

--- src/FBT RSS Background Tabs/ViewController.swift
+++ src/FBT RSS Background Tabs/ViewController.swift
@@ -26,19 +26,5 @@ class ViewController: NSViewController {
             }
         }
     }
-
-    @IBAction func openDonateLink(_ sender: AnyObject?) {
-        let url = URL(string: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=RD9TUVZFBUEWC&source=url")!
-        if NSWorkspace.shared.open(url) {
-            print("default browser was successfully opened")
-        }
-    }
-    
-    @IBAction func openDonateCryptoLink(_ sender: AnyObject?) {
-        let url = URL(string: "https://commerce.coinbase.com/checkout/06e3c570-fee4-42ac-bdd2-5aa6cd1f8569")!
-        if NSWorkspace.shared.open(url) {
-            print("default browser was successfully opened")
-        }
-    }
     
 }
