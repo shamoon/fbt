061ad917458f6dd262b437cf933678eac60199b4
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-12-22 13:05:43 -0800

    Feedly no longer reliably indicates current article
    Closes #15

--- src/FBT Extension/script.js
+++ src/FBT Extension/script.js
@@ -17,70 +17,60 @@ window.addEventListener('keydown', function(event) {
 
   // Catch the shortcut key, but ignore modified key presses.
   if (event.key == shortcutKey && !event.ctrlKey && !event.metaKey) {
-     var currents = document.getElementsByClassName('inlineFrame--selected');
-     if (debug) console.log('[feedly background tabs] currents found w/ "inlineFrame--selected": ');
-     if (debug) console.log(currents);
-     // if at first you do not succeed... cycle through Feedly's different current entry classes
-     if (currents.length == 0) {
-         currents = document.getElementsByClassName('selected inlineFrame');
-         if (debug) console.log('[feedly background tabs] currents found w/ "selected inlineFrame": ');
-         if (debug) console.log(currents);
+      var currentArticle;
+      var articles = document.getElementsByClassName('u100Entry');
+      if (debug) console.log('[feedly background tabs] articles found w/ "u100Entry": ');
+      if (debug) console.log(articles);
+      for (var i = 0; i < articles.length; i++) {
+          article = articles[i];
+          var rect = article.getBoundingClientRect();
+          if (rect.top < window.innerHeight && rect.bottom >= 0) {
+              // partially visible, but keep going to find last one
+              currentArticle = article;
+          }
      }
-     if (currents.length == 0) {
-        var currents = document.getElementsByClassName('selected entry');
-        if (debug) console.log('[feedly background tabs] currents found w/ "selected entry": ');
-        if (debug) console.log(currents);
-     }
-     if (currents.length == 0) {
-        currents = document.getElementsByClassName('selectedEntry');
-        if (debug) console.log('[feedly background tabs] currents found w/ "selectedEntry": ');
-        if (debug) console.log(currents);
-     }
-     if (currents.length == 0) {
-        currents = document.getElementsByClassName('u100Entry');
-        if (debug) console.log('[feedly background tabs] currents found w/ "u100Entry": ');
-        if (debug) console.log(currents);
-     }
-     var current = currents[0];
-     if (debug) console.log('[feedly background tabs] current found:');
-     if (debug) console.log(current);
+      
      var url = '';
-     if (!current.hasAttribute('data-alternate-link')) {
-        var currentChild;
-        // try anchors
-        var children = current.getElementsByTagName('a');
-        for (var i = 0; i < children.length; i++) {
-            currentChild = children[i];
-            if (currentChild.classList.contains("entryTitle") || currentChild.classList.contains("Article__title--read")) {
-                if (debug) console.log('[feedly background tabs] found url on anchor:');
-                if (debug) console.log(currentChild);
-                url = currentChild.getAttribute('href');
-            }
-        }
-        // look for child data-alternate-link
-        if (!url) {
-             children = current.getElementsByTagName('*');
-             for (var i = 0; i < children.length; i++) {
+      if (currentArticle) {
+         if (debug) console.log('[feedly background tabs] current article:');
+         if (debug) console.log(currentArticle);
+         if (!currentArticle.hasAttribute('data-alternate-link')) {
+            var currentChild;
+            // try anchors
+            var children = currentArticle.getElementsByTagName('a');
+            for (var i = 0; i < children.length; i++) {
                 currentChild = children[i];
-                if (currentChild.hasAttribute('data-alternate-link')) {
-                   if (debug) console.log('[feedly background tabs] found url on child:');
-                   if (debug) console.log(currentChild);
-                   url = currentChild.getAttribute('data-alternate-link');
-                   break;
+                if (currentChild.classList.contains("entryTitle") || currentChild.classList.contains("Article__title--read")) {
+                    if (debug) console.log('[feedly background tabs] found url on anchor:');
+                    if (debug) console.log(currentChild);
+                    url = currentChild.getAttribute('href');
                 }
+            }
+            // look for child data-alternate-link
+            if (!url) {
+                 children = currentArticle.getElementsByTagName('*');
+                 for (var i = 0; i < children.length; i++) {
+                    currentChild = children[i];
+                    if (currentChild.hasAttribute('data-alternate-link')) {
+                       if (debug) console.log('[feedly background tabs] found url on child:');
+                       if (debug) console.log(currentChild);
+                       url = currentChild.getAttribute('data-alternate-link');
+                       break;
+                    }
+                 }
              }
+         } else {
+            url = currentArticle.getAttribute('data-alternate-link');
          }
-     } else {
-        url = current.getAttribute('data-alternate-link');
-     }
-     if (debug) console.log('[feedly background tabs] url found: ' + url);
-     if (!url || url == '') {
-        return;
-     }
-     event.stopPropagation();
-     event.stopImmediatePropagation();
-     event.preventDefault();
-     safari.extension.dispatchMessage('openFeedlyBackgroundTab', { "url" : url });
+         if (debug) console.log('[feedly background tabs] url found: ' + url);
+         if (!url || url == '') {
+            return;
+         }
+         event.stopPropagation();
+         event.stopImmediatePropagation();
+         event.preventDefault();
+         safari.extension.dispatchMessage('openFeedlyBackgroundTab', { "url" : url });
+      }
   }
 }, true);
 
